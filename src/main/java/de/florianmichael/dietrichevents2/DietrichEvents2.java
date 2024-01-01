2696e7dd54e0227340aff9a6018b767294718b0c
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-01 15:21:46 +0100

    Updated metadata

--- src/main/java/de/florianmichael/dietrichevents2/DietrichEvents2.java
+++ src/main/java/de/florianmichael/dietrichevents2/DietrichEvents2.java
@@ -1,12 +1,12 @@
 /*
  * This file is part of DietrichEvents2 - https://github.com/FlorianMichael/DietrichEvents2
- * Copyright (C) 2023-2024 FlorianMichael/EnZaXD and contributors
+ * Copyright (C) 2023-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
