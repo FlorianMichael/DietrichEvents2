40a819cc4766bfd43a66d242353de97a220bbded
FlorianMichael <git@florianmichael.de>
2025-12-25 20:46:00 +0100

    Update conventions plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
     }
 
     plugins {
-        id("de.florianmichael.baseproject.BaseProject") version "1.2.8"
+        id("de.florianmichael.baseproject.BaseProject") version "1.3.2"
     }
 }
 
